ca246505a086cd8e51b4a02b5ce88c6b27e59ee5
Bradley Grainger <bgrainger@gmail.com>
2017-08-15 22:30:50 -0700

    Add examples of default literal.

---
+++ DefaultLiteral.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+
+namespace CSharp71
+{
+	public static class DefaultLiteral
+	{
+		public static void Locals()
+		{
+			// initialize struct, class, or built-in
+			ValueTuple<string, int> tuple = default;
+			IReadOnlyCollection<string> collection = default;
+			int i = default;
+
+			// can initialize constants
+			const int j = default;
+
+			// C# 7.0
+			ValueTuple<string, int> tuple1 = default(ValueTuple<string, int>);
+			var tuple2 = default(ValueTuple<string, int>);
+		}
+
+		public static void ConditionalExpression(string value)
+		{
+			var tuple = value == null ? default : (value, value.Length);
+		}
+
+		public static ValueTuple<string, int> GetValue()
+		{
+			// C# 7.0: return default(ValueTuple<string, int>);
+			return default;
+		}
+
+		public static void OptionalParameters(ValueTuple<string, int> tuple = default)
+		{
+			// C# 7.0: ValueTuple<string, int> tuple = default(ValueTuple<string, int>)
+		}
+
+		public static void ArrayInitializers()
+		{
+			var array = new[]
+			{
+				default,
+				// C# 7.0: default(ValueTuple<string, int>),
+				("name", 42),
+			};
+		}
+
+		public static void MethodCall()
+		{
+			ConditionalExpression(default);
+			OptionalParameters(default);
+		}
+
+		public static void Comparison(string value)
+		{
+			if (default == value) { }
+			if (value == default) { }
+			if (value is default) { }
+		}
+
+		public static void As()
+		{
+			// only allowed for reference types
+			// warning CS0458: The result of the expression is always 'null' of type 'IReadOnlyCollection<string>'
+			var collection = default as IReadOnlyCollection<string>;
+		}
+
+		public static void SwitchStatement(string value)
+		{
+			switch (value)
+			{
+			case " ":
+				Console.WriteLine("a single space");
+				break;
+
+			case "":
+				Console.WriteLine("is empty");
+				break;
+
+			case (default):
+				// case default:  warning CS8313: Did you mean to use the default switch label (`default:`) rather than `case default:`? If you really mean to use the default literal, consider `case (default):` or another literal (`case 0:` or `case null:`) as appropriate.
+				Console.WriteLine("is null");
+				break;
+
+			default:
+				Console.WriteLine("something else");
+				break;
+			}
+		}
+
+		public static void Illegal()
+		{
+			// error CS8315: Operator '==' is ambiguous on operands 'default' and 'default'
+			// if (default == default);
+			// NOTE: "if (null == null)" is allowed
+
+			// error CS0023: Operator 'is' cannot be applied to operand of type 'default'
+			// if (default is string);
+
+			// error CS0815: Cannot assign default to an implicitly-typed variable
+			// var i = default;
+
+			// error CS0155: The type caught or thrown must be derived from System.Exception
+			// throw default;
+			// NOTE: "throw default(Exception);" and "throw null;" compile and throw NullReferenceException at runtime; compiler team closed this loophole
+		}
+	}
+}
