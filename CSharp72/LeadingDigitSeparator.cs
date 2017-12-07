1955ba51854db1f0847eb34e5beab4d34a2587a5
Bradley Grainger <bgrainger@gmail.com>
2017-12-06 21:55:31 -0800

    Add leading digit separator.

---
+++ CSharp72/LeadingDigitSeparator.cs
@@ -0,0 +1,23 @@
+namespace CSharp72
+{
+    public class LeadingDigitSeparator
+    {
+		public void Previous()
+		{
+			// _ was allowed as digit separator in numbers
+			var binary = 0b1100_0010_1101;
+			var hex = 0xDEAD_BEEF;
+			var base10 = 123_456_789;
+		}
+
+		public void Now()
+		{
+			// underscore can now immediately follow the prefix
+			var binary = 0b_1100_0010_1101;
+			var hex = 0x_DEAD_BEEF;
+
+			// can't have leading underscore on non-prefixed number; this is an identifier
+			// var base10 = _123_456_789;
+		}
+    }
+}
