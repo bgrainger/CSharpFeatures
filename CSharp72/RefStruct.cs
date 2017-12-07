3943b8476771ef345b540530dca5a08830e88b1d
Bradley Grainger <bgrainger@gmail.com>
2017-12-07 13:13:26 -0800

    Add ref struct.

---
+++ CSharp72/RefStruct.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharp72
+{
+    public class Examples
+    {
+		public void UseInMethod()
+		{
+			var rs = new RefStruct();
+		}
+
+		public void CannotBox()
+		{
+			var rs = new RefStruct();
+
+			// error CS0029: Cannot implicitly convert type 'CSharp72.RefStruct' to 'object'
+			// object boxed = rs;
+			// Console.WriteLine("{0}", rs);
+			// Console.WriteLine($"{rs}");
+		}
+
+		public IEnumerable<int> CannotUseInIterator()
+		{
+			var rs = new RefStruct(1);
+			if (DateTime.Now.Second % 2 == 0)
+				yield return rs.Value;
+			else
+				rs = new RefStruct(2);
+			//  error CS4013: Instance of type 'RefStruct' cannot be used inside a nested function, query expression, iterator block or async method
+			// yield return rs.Value;
+		}
+
+		public async Task CannotUseInAsyncMethod()
+		{
+			// error CS4012: Parameters or locals of type 'RefStruct' cannot be declared in async methods or lambda expressions.
+			// var rs = new RefStruct(1);
+		}
+
+		public void CannotCaptureInLambda()
+		{
+			var rs = new RefStruct(1);
+
+			// error CS8175: Cannot use ref local 'rs' inside an anonymous method, lambda expression, or query expression
+			// Enumerable.Range(1, 10).Where(x => x == rs.Value);
+		}
+
+		// enables Span<T> with any kind of memory
+		// enables composing Span<T> in other types
+	}
+
+	public ref struct RefStruct
+	{
+		public RefStruct(int value) => Value = value;
+
+		public int Value { get; }
+	}
+}
