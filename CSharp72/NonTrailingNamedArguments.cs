e2ac7410a86cb6765e163136381bd76a21df2804
Bradley Grainger <bgrainger@gmail.com>
2017-12-06 21:44:56 -0800

    Add non-trailing named arguments.

---
+++ CSharp72/NonTrailingNamedArguments.cs
@@ -0,0 +1,29 @@
+using System.IO;
+
+namespace CSharp72
+{
+	public class NonTrailingNamedArguments
+    {
+		public void Previous()
+		{
+			// option 1: don't name arguments (loses clarity)
+			SomeMethod(@"C:\Temp\test.txt", false, true, FileShare.Read, FileAccess.ReadWrite);
+
+			// option 2: name all arguments to right (verbose)
+			SomeMethod(@"C:\Temp\test.txt", truncate: false, createIfMissing: true, fileShare: FileShare.Read, fileAccess: FileAccess.ReadWrite);
+		}
+
+		public void Now()
+		{
+			// name non-trailing arguments
+			SomeMethod(@"C:\Temp\test.txt", truncate: false, createIfMissing: true, FileShare.Read, FileAccess.ReadWrite);
+
+			// can't swap argument order if positional parameters follow
+			// SomeMethod(@"C:\Temp\test.txt", createIfMissing: true, truncate: false, FileShare.Read, FileAccess.ReadWrite);
+		}
+
+		public void SomeMethod(string filePath, bool truncate, bool createIfMissing, FileShare fileShare, FileAccess fileAccess)
+		{
+		}
+    }
+}
