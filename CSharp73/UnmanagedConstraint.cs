bc4535d8e6c74d9e1a9651583255d5e07d5b410c
Bradley Grainger <bgrainger@gmail.com>
2018-05-04 15:26:37 -0700

    Add "where T : unmanaged".

---
+++ CSharp73/UnmanagedConstraint.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+namespace CSharp73
+{
+	class UnmanagedConstraint
+	{
+		public static unsafe int Hash<T>(in T value) where T : unmanaged
+		{
+			fixed (T* ptr = &value)
+			{
+				uint hash = 5381;
+				byte* p = (byte*) ptr;
+				for (int i = 0; i < sizeof(T); i++)
+					hash = hash * 33 + p[i];
+				return (int) hash;
+			}
+		}
+
+		public static void Example()
+		{
+			Console.WriteLine(Hash(42));
+			Console.WriteLine(Hash(new Point { X = 3, Y = 7 }));
+		}
+	}
+
+	struct Point
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+	}
+}
