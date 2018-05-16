2b73dd653827046cedd574dc1e350b311fe22c60
Bradley Grainger <bgrainger@gmail.com>
2018-05-16 15:12:18 -0700

    Demonstrate unsafe code.

--- CSharp73/IndexingFixedFields.cs
+++ CSharp73/IndexingFixedFields.cs
@@ -9,11 +9,11 @@ namespace CSharp73
 	/// <summary>
 	/// https://github.com/dotnet/csharplang/blob/master/proposals/csharp-7.3/indexing-movable-fixed-fields.md
 	/// </summary>
-	unsafe class IndexingFixedFields
+	class IndexingFixedFields
 	{
 		FixedStruct m_fs;
 
-		public void CSharp7()
+		public unsafe void CSharp7()
 		{
 			// can access field directly
 			var value2 = m_fs.FixedField;
@@ -25,11 +25,15 @@ namespace CSharp73
 			}
 		}
 
-		public void CSharp73()
+		public unsafe void CSharp73()
 		{
 			// direct access without pinning
 			// error CS1666: You cannot use fixed size buffers contained in unfixed expressions. Try using the fixed statement.
 			var value = m_fs.FixedArray[5];
+
+			// NOTE: method still needs to be 'unsafe'
+			// error CS0214: Pointers and fixed size buffers may only be used in an unsafe context
+			var outOfBoundsRead = m_fs.FixedArray[15];
 		}
 	}
 }
