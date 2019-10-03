09c630ea6786f77f63ded909cbd009eb03b87e83
Bradley Grainger <bgrainger@gmail.com>
2019-10-02 19:21:27 -0700

    Add more notes on readonly.

--- CSharp8Framework/ReadonlyStructMember.cs
+++ CSharp8Framework/ReadonlyStructMember.cs
@@ -13,10 +13,12 @@ namespace CSharp8Framework
 		{
 			X = x;
 			Y = y;
+			m_label = null;
+			Label3 = null;
 		}
 
-		public int X { get; }
-		public int Y { get; }
+		public int X { get; set; }
+		public int Y { get; set; }
 
 		// regular member
 		public double Distance => Math.Sqrt(X * X + Y * Y);
@@ -29,5 +31,22 @@ namespace CSharp8Framework
 
 		// CS8656: Call to non-readonly member 'Point.Distance.get' from a 'readonly' member results in an implicit copy of 'this'.
 		public readonly string Description => $"{ToString()} is {Distance} from the origin";
+
+		// You can--and should--apply readonly to non-auto-implemented getters, or just replace them with auto properties.
+		public string Label
+		{
+			readonly get => m_label;
+			set => m_label = value;
+		}
+		string m_label;
+
+		public string Label2
+		{
+			get => m_label;
+			readonly set => Console.WriteLine($"You tried to set it to {value}");
+		}
+
+		// You can do this, but it's redundant; the compiler applies the readonly attribute automatically.
+		public string Label3 { readonly get; set; }
 	}
 }
