001b004b4d0156c679af9efd64f944455e0a86f7
Bradley Grainger <bgrainger@gmail.com>
2020-11-11 06:24:08 -0800

    Add init-only properties.

---
+++ CSharp9Framework/InitOnlyProperties.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Runtime.CompilerServices
+{
+	public class IsExternalInit { }
+}
+
+namespace CSharp9Framework
+{
+	class InitOnlyProperties
+	{
+		class Poco
+		{
+			public string Name { get; set; }
+			public int Age { get; set; }
+		}
+
+		public void Mutable()
+		{
+			var bob = new Poco
+			{
+				Name = "Bob",
+				Age = 25,
+			};
+
+			// mutable
+			bob.Name = "John";
+		}
+
+		class PocoInit
+		{
+			public string Name { get; init; }
+			public int Age { get; init; }
+		}
+
+		public void NotMutable()
+		{
+			var bob = new PocoInit
+			{
+				Name = "Bob",
+				Age = 25,
+			};
+
+			// immutable
+			// error CS8852: Init-only property or indexer 'X' can only be assigned in an object initializer, or on 'this' or 'base' in an instance constructor or an 'init' accessor.
+			// bob.Name = "John";
+		}
+
+		/*
+		 * An instance property containing an init accessor is considered settable in the following circumstances, except when in a local function or lambda:
+		 * During an object initializer
+		 * During a with expression initializer
+		 * Inside an instance constructor of the containing or derived type, on this or base
+		 * Inside the init accessor of any property, on this or base
+		 * Inside attribute usages with named parameters
+		 */
+		class SetReadonlyProperties
+		{
+			readonly string _name; // the "init;" syntax generates a readonly field
+
+			public string Name
+			{
+				get => _name;
+				init => _name = value ?? throw new ArgumentNullException(nameof(value));
+			}
+		}
+
+		class Derived : SetReadonlyProperties
+		{
+			readonly int _age;
+
+			public int Age
+			{
+				get => _age;
+				init
+				{
+					_age = value;
+					Name = value.ToString();
+				}
+			}
+		}
+
+		public void UseDerived()
+		{
+			var derived = new Derived { Age = 10 };
+		}
+	}
+}
