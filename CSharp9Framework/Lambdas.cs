844c069315ce0e00166c46b9f418c6562fa73f79
Bradley Grainger <bgrainger@gmail.com>
2020-11-17 11:27:57 -0800

    Static anonymous methods aren't new.

--- CSharp9Framework/Lambdas.cs
+++ CSharp9Framework/Lambdas.cs
@@ -3,20 +3,11 @@ using System.Linq;
 
 namespace CSharp9Framework
 {
-	public class StaticLambdas
+	public class Lambdas
 	{
 		public void StaticLambda(IEnumerable<int> numbers)
 		{
 			numbers.Select(static x => x % 3 is 0 || x % 5 is 0).ToList();
-		}
-
-		public void StaticAnonymousMethod(IEnumerable<int> numbers)
-		{
-			var threes = numbers.Select(IsMultipleOfThree);
-			var fives = numbers.Select(IsMultipleOfFive);
-
-			static bool IsMultipleOfThree(int x) => x % 3 == 0;
-			static bool IsMultipleOfFive(int x) => x % 5 == 0;
 		}
 
 		public void LambdaDiscards(IEnumerable<object> first, IEnumerable<object> second)
