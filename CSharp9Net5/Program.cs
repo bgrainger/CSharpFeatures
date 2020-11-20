17c108d4a92d24c4f7587323e23926590842635c
Bradley Grainger <bgrainger@gmail.com>
2020-11-20 08:28:48 -0800

    Add non-statements to top-level program.

--- CSharp9Net5/Program.cs
+++ CSharp9Net5/Program.cs
@@ -1,3 +1,19 @@
 using System;
 
 Console.WriteLine("Hello, .NET 5.0!");
+
+// local functions
+int Double(int x) => x * 2;
+
+class AClass
+{
+	public void AMethod() { }
+}
+
+namespace ANamespace
+{
+	class AClass
+	{
+		public void AMethod() { }
+	}
+}
