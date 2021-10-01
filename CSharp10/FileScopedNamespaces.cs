e191710500c5e12e8100d288c800c2c5e3c4559c
Bradley Grainger <bgrainger@gmail.com>
2021-10-01 14:55:18 -0700

    Demonstrate file-scoped namespaces.

---
+++ CSharp10/FileScopedNamespaces.cs
@@ -0,0 +1,10 @@
+namespace CSharp10;
+
+internal class FileScopedNamespaces
+{
+}
+
+// can't do this: error CS8955: Source file can not contain both file-scoped and normal namespace declarations.
+// namespace GlobalOrNested?
+// {
+// }
