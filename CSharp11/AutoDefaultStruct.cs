6462d930eade77890b54a244d6decc12d5e3b25b
Bradley Grainger <bgrainger@gmail.com>
2022-12-06 15:36:27 -0800

    Add auto-default struct initialization.

---
+++ CSharp11/AutoDefaultStruct.cs
@@ -0,0 +1,14 @@
+namespace CSharp11;
+
+struct AutoDefaultStruct
+{
+	public AutoDefaultStruct()
+	{
+		Name = "No name";
+
+		// previously: error CS0843: Auto-implemented property 'AutoDefaultStruct.Age' must be fully assigned before control is returned to the caller.
+	}
+
+	public string Name { get; }
+	public int Age { get; }
+}
